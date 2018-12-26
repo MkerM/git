7e2bffff36e24e2cbccbf59b01e361fffb28a5a6
MkerM <Mker.mohamedone@gamil.com>
2018-12-26 14:39:31 -0800

    helpp

--- src/git/Git.java
+++ src/git/Git.java
@@ -5,7 +5,7 @@ public class Git {
 
     public static void main(String[] args) {
         System.out.println("Hellow Word");
-        System.out.println("first shit");
+        System.out.println("first shit 2nd");
         
     }
     
