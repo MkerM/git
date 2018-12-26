43d54322efde69533672874216ca3ec1619c9308
MkerM <Mker.mohamedone@gamil.com>
2018-12-26 04:32:26 -0800

    Lab

---
+++ src/git/Git.java
@@ -0,0 +1,12 @@
+
+package git;
+
+public class Git {
+
+    public static void main(String[] args) {
+        System.out.println("Hellow Word");
+        System.out.println("first shit");
+        
+    }
+    
+}
