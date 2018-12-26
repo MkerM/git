c1477ff378271863bf073f030c3d315e65485421
MkerM <46159256+MkerM@users.noreply.github.com>
2018-12-26 05:17:29 -0800

    Update Git.java

--- src/git/Git.java
+++ src/git/Git.java
@@ -5,7 +5,7 @@ public class Git {
 
     public static void main(String[] args) {
         System.out.println("Hellow Word");
-        System.out.println("first shit 55nd");
+        System.out.println("first shit 5nd");
         
     }
     
